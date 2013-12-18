ddf94d16b6a7553e6881fdf1241a26b259673df5
brendonfavero <brendonfavero@gmail.com>
2013-12-18 14:39:30 +1100

    ckeditor update

--- addons/ppNews/lib/ckeditor/config.js
+++ addons/ppNews/lib/ckeditor/config.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -8,4 +8,27 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.contentsCss = '/ps/global.css';
+    config.extraPlugins = 'MediaEmbed';
+	
+	config.toolbar_Full =
+[
+    ['Source','-','Save','NewPage','Preview','-','Templates'],
+    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+    ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+    ['BidiLtr', 'BidiRtl'],
+    '/',
+    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+    ['Link','Unlink','Anchor'],
+    ['Image','MediaEmbed','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+    '/',
+    ['Styles','Format','Font','FontSize'],
+    ['TextColor','BGColor'],
+    ['Maximize', 'ShowBlocks','-','About']
+];
+
 };
+CKEDITOR.plugins.addExternal('mediaembed', '/addons/ampseCustom/lib/ckeditor/plugins/mediaembed/')
